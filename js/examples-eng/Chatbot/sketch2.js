e6de6eb4d4575bd5e0a6e9b204b7c21fd8f9f654
Alp <alp@collectivat.cat>
2019-11-07 13:09:57 +0100

    chatbot

---
+++ js/examples-eng/Chatbot/sketch2.js
@@ -0,0 +1,59 @@
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+
+// Number Guessing Chatbot
+// Edited Video: https://www.youtube.com/watch?v=zGe1m_bLOFk
+
+var myRec = new p5.SpeechRec('en-US'); // new P5.SpeechRec object
+	myRec.continuous = true; // do continuous recognition
+	myRec.interimResults = true; // allow partial recognition (faster, less accurate)
+
+var bot;
+
+function setup() {
+  // graphics stuff:
+		createCanvas(600, 400);
+		background(255, 255, 255);
+		fill(0, 0, 0, 255);
+
+  bot = new RiveScript();
+  bot.loadFile("brain.rive").then(brainReady).catch(brainError); 
+
+  function brainReady() {
+    console.log('Chatbot ready!');
+    bot.sortReplies();
+    let num = floor(random(10)) + 1;
+    console.log(num);
+    bot.reply('local-user', 'set ' + num);
+  }
+
+  function brainError() {
+    console.log('Chatbot error!')
+  }
+
+  textSize(32);
+	textAlign(CENTER);
+	text("pick a number", width/2, height/2);
+	myRec.onResult = parseResult;
+	myRec.start();
+}
+
+function chat(input) {
+    bot.reply("local-user", input).then(function(reply) {
+      text(reply, width/2, height/2);
+    });
+  }
+
+function parseResult()
+	{
+		// recognition system will often append words into phrases.
+		// so hack here is to only use the last word:
+		var mostrecentword = myRec.resultString.split(' ').pop();
+
+		console.log(mostrecentword);
+
+    background(192, 255, 192);
+    
+    setTimeout(chat(mostrecentword), 1500); 
+	}
